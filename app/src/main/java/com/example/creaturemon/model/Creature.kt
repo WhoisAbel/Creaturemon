013d9fb3a9675e09fa512ece623e53186c49c529
Abolfazl Kalamati <abolfazl.kalamati@yahoo.com>
2022-03-29 12:35:24 +0430

    Building the models (#2)

--- app/src/main/java/com/example/creaturemon/model/Creature.kt
+++ app/src/main/java/com/example/creaturemon/model/Creature.kt
@@ -1,3 +1,13 @@
 package com.example.creaturemon.model
 
-class Creature
+data class Creature(
+    val creatureAttributes: CreatureAttributes,
+    val hitPoints: Int = 0,
+    val name: String = "",
+    val drawable: Int = 0
+) {
+    private val hitPointsCalculate =
+        creatureAttributes.intelligence * 5 + creatureAttributes.strength * 4 + creatureAttributes.endurance * 3
+
+    fun creatureGenerator() = Creature(creatureAttributes, hitPointsCalculate, name, drawable)
+}
